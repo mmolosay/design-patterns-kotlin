fe86435a6b18c932b88691bfd65d29bc976efbf8
ordo <ordogod00@gmail.com>
2019-09-24 18:31:16 +0300

    Facade pattern implementation was redisigned

--- src/facade/Facade.kt
+++ src/facade/Facade.kt
@@ -1,85 +1,26 @@
 package facade
 
-interface SmartPowerBehavior {
-    // toggle power distantly
-    fun on()
-    fun off()
-}
+class PC  {
+    fun start() = this
+        .makeLoudNoise()
+        .turnLightsOn()
+        .greetUser()
+        .prepareDesktop()
+        .displayDesktop()
 
-interface SmartUseBehavior {
-    // use item distantly
-    fun use()
-}
-
-abstract class SmartTech(private val name: String) : SmartPowerBehavior, SmartUseBehavior {
-
-    override fun on() = println("$name: on")
-
-    override fun off() = println("$name: off")
-
-    abstract override fun use()
-}
-
-class SmartHouse {
-
-    val innerLight = object : SmartTech("House light") {
-        override fun use() {}
+    private fun makeLoudNoise() : PC = apply {
+        println("Starting to noise loudly...")
     }
-    val gardenLight = object : SmartTech("Garden light") {
-        override fun use() {}
+    private fun turnLightsOn() : PC = apply {
+        println("Turning the lights on...")
     }
-    val teapot = object : SmartTech("Teapot") {
-        override fun use() = println("Water is boiling.")
+    private fun greetUser() : PC = apply {
+        println("Greeting the user...")
     }
-    val toster = object : SmartTech("Toster") {
-        override fun use() = println("Toasts are ready!")
+    private fun prepareDesktop() : PC = apply {
+        println("Preparing the desktop...")
     }
-    val acousticSystem = object : SmartTech("Acoustic system") {
-        override fun use() = println("Only good music plays in this house.")
+    private fun displayDesktop() : PC = apply {
+        println("PC is ready!")
     }
-}
-
-class UniqueRemoteController(private val house: SmartHouse) {
-
-    fun atHomeArrival() =
-        with (house) {
-            println("~~~~~~ You arrive home ~~~~~~")
-            gardenLight.on()
-            innerLight.on()
-            acousticSystem.on()
-            acousticSystem.use()
-            println()
-        }
-
-    fun atHomeDeparture() =
-        with (house) {
-            println("~~~~~~ You leaving home ~~~~~~")
-            gardenLight.off()
-            innerLight.off()
-            acousticSystem.off()
-            println()
-        }
-
-    fun atWakeUp() =
-        with (house) {
-            println("~~~~~~ You wake up ~~~~~~")
-            teapot.on()
-            teapot.use()
-            teapot.off()
-            toster.on()
-            toster.use()
-            toster.off()
-            acousticSystem.on()
-            acousticSystem.use()
-            println()
-        }
-
-    fun atSleep() =
-        with (house) {
-            println("~~~~~~ You go to sleep ~~~~~~")
-            acousticSystem.off()
-            gardenLight.off()
-            innerLight.off()
-            println()
-        }
 }
