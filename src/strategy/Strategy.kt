cfa2ea4dfee4a7f10695f660c3c13c5438d6bcb4
ordo <ordogod00@gmail.com>
2019-09-30 12:54:49 +0300

    SAM conversion simulation implemented for QuackBehavior interface of Strategy pattern

--- src/strategy/Strategy.kt
+++ src/strategy/Strategy.kt
@@ -3,6 +3,12 @@ package strategy
 interface QuackBehavior {
 
     fun quack(): String
+
+    // not necessary, just simulates SAM conversion
+    companion object {
+        inline operator fun invoke(crossinline block: () -> String) =
+            object : QuackBehavior { override fun quack(): String = block() }
+    }
 }
 
 class Duck(private var quackBehavior: QuackBehavior) {
